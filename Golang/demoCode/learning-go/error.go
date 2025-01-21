325383a898dda640756633c39f477cff9cf0bee4
Xu <whitenight.xiaxu@gmail.com>
2025-01-21 15:03:55 +0800

    Update error.go

--- Golang/demoCode/learning-go/error.go
+++ Golang/demoCode/learning-go/error.go
@@ -1,23 +1,29 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
+import "errors"
 
-func handleError() {
-	fmt.Printf("hello world!")
-
-
+func calcRemainderAndMod(numerator, denominator int) (int, int, error) {
+	if denominator == 0 {
+		return 0, 0, errors.New("denominator is 0")
+	}
+	return numerator / denominator, numerator % denominator, nil
 }
 
-type error interface {
-	Error() string
-}
-
-func doubleEven(i int) (int, error){
-	if i %2 != 0{
-		return 0, errors.New("only even number are processed")
+func doubleEven(i int) (int, error) {
+	if i%2 != 0 {
+		return 0, errors.New("only even numbers are processed")
 	}
 	return i * 2, nil
 }
+
+// Errors are values
+
+// Wrapping errors
+
+//is and as
+
+//Wrapping errors with defer
+
+//panic and recover
+
+// Getting a stack trace from an eoor
